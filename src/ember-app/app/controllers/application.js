54ffb256d6b808d932f542723a1bc2eb3b2ca472
Flexberry-man <mail@flexberry.net>
2024-01-13 17:08:40 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/application.js
+++ src/ember-app/app/controllers/application.js
@@ -32,7 +32,7 @@ export default Controller.extend({
               link: 'i-i-s-cursed-сотрудник-l',
               caption: i18n.t('forms.application.sitemap.cursed.сотрудники.i-i-s-cursed-сотрудник-l.caption'),
               title: i18n.t('forms.application.sitemap.cursed.сотрудники.i-i-s-cursed-сотрудник-l.title'),
-              icon: 'briefcase',
+              icon: 'tags',
               children: null
             }]
           }, {
@@ -43,19 +43,18 @@ export default Controller.extend({
               link: 'i-i-s-cursed-заказ-l',
               caption: i18n.t('forms.application.sitemap.cursed.информация-о-заказе.i-i-s-cursed-заказ-l.caption'),
               title: i18n.t('forms.application.sitemap.cursed.информация-о-заказе.i-i-s-cursed-заказ-l.title'),
-              icon: 'file',
               children: null
             }, {
               link: 'i-i-s-cursed-документ-l',
               caption: i18n.t('forms.application.sitemap.cursed.информация-о-заказе.i-i-s-cursed-документ-l.caption'),
               title: i18n.t('forms.application.sitemap.cursed.информация-о-заказе.i-i-s-cursed-документ-l.title'),
-              icon: 'edit',
+              icon: 'book',
               children: null
             }, {
               link: 'i-i-s-cursed-накладная-l',
               caption: i18n.t('forms.application.sitemap.cursed.информация-о-заказе.i-i-s-cursed-накладная-l.caption'),
               title: i18n.t('forms.application.sitemap.cursed.информация-о-заказе.i-i-s-cursed-накладная-l.title'),
-              icon: 'briefcase',
+              icon: 'paperclip',
               children: null
             }]
           }, {
@@ -66,31 +65,30 @@ export default Controller.extend({
               link: 'i-i-s-cursed-товар-l',
               caption: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-товар-l.caption'),
               title: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-товар-l.title'),
-              icon: 'building',
               children: null
             }, {
               link: 'i-i-s-cursed-склад-l',
               caption: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-склад-l.caption'),
               title: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-склад-l.title'),
-              icon: 'table',
+              icon: 'building',
               children: null
             }, {
               link: 'i-i-s-cursed-ряд-l',
               caption: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-ряд-l.caption'),
               title: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-ряд-l.title'),
-              icon: 'tasks',
+              icon: 'list',
               children: null
             }, {
               link: 'i-i-s-cursed-этаж-l',
               caption: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-этаж-l.caption'),
               title: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-этаж-l.title'),
-              icon: 'file',
+              icon: 'building',
               children: null
             }, {
               link: 'i-i-s-cursed-ячейка-l',
               caption: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-ячейка-l.caption'),
               title: i18n.t('forms.application.sitemap.cursed.товар-на-складе.i-i-s-cursed-ячейка-l.title'),
-              icon: 'chart bar',
+              icon: 'book',
               children: null
             }]
           }]
